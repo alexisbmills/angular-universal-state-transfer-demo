30516179ef8cca56058d1b39c6d554c564163222
Alexis Bradley-Mills <alexis.bradleymills@gmail.com>
2019-02-13 12:09:28 +0800

    Reintroduce flickering by adding image. Some code simplification.

--- server/api/users.ts
+++ server/api/users.ts
@@ -7,36 +7,66 @@ const USERS_FIXTURES: User[] = [
     firstName: 'John',
     lastName: 'Public',
     email: 'john.public@gmail.com',
+    image: {
+      width: 200,
+      height: 300,
+      url: 'https://picsum.photos/200/300?image=0'
+    }
   },
   {
     id: 'uid4-1234-5678-9102',
     firstName: 'Elaine',
     lastName: 'Private',
     email: 'helen.oftroy@gmail.com',
+    image: {
+      width: 200,
+      height: 300,
+      url: 'https://picsum.photos/200/300?image=1'
+    }
   },
   {
     id: 'uid4-1234-5678-9103',
     firstName: 'Saul',
     lastName: 'Good',
     email: 'saul.good@gmail.com',
+    image: {
+      width: 200,
+      height: 300,
+      url: 'https://picsum.photos/200/300?image=2'
+    }
   },
   {
     id: 'uid4-1234-5678-9104',
     firstName: 'Peter',
     lastName: 'Yellow',
     email: 'p.yellow@gmail.com',
+    image: {
+      width: 200,
+      height: 300,
+      url: 'https://picsum.photos/200/300?image=3'
+    }
   },
   {
     id: 'uid4-1234-5678-9105',
     firstName: 'Kylie',
     lastName: 'Underwood',
     email: 'underwood.k@gmail.com',
+    image: {
+      width: 200,
+      height: 300,
+      url: 'https://picsum.photos/200/300?image=4'
+    }
   },
   {
     id: 'uid4-1234-5678-9106',
     firstName: 'Martin',
     lastName: 'Starter',
     email: 'start@gmail.com',
+    image: {
+      width: 200,
+      height: 300,
+      url: 'https://picsum.photos/200/300?image=5'
+    }
   }
 ];
 
--- src/app/module/users/services/user.ts
+++ src/app/module/users/services/user.ts
@@ -1,6 +1,13 @@
+export interface Image {
+  width: number;
+  height: number;
+  url: string;
+}
+
 export interface User {
   id: string;
   firstName: string;
   lastName: string;
   email: string;
+  image: Image;
 }
